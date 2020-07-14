e1432311d18c7821675b5175ddeb3892cecdd789
JoAnna Park <joannabpark1@gmail.com>
2020-07-14 22:17:40 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,41 @@
-def create_an_empty_array
-  
+def 
+  create_an_empty_array 
+  []
 end
 
-def create_an_array
-  
+def 
+  create_an_array
+  ["one", "two", "three", "four"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[5]
 end
 
 def update_element_from_index(array, index_number, element)
-
+element[4] = "totally"
 end
